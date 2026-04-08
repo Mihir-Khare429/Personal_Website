5ff87365769c6b1b069f8c83d0fe790c98dfff3b
Mihir_Khare429 <42797667+Mihir-Khare429@users.noreply.github.com>
2026-04-08 00:16:59 -0400

    Set base path for personal website

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  base: "/Personal_Website/",
   vite: {
     plugins: [tailwindcss()],
     site: 'https://mihir-khare429.github.io/Portfolio/',
